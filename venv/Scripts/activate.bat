90b91bde7fb9f13625ec57a3a7438d2d337f9088
DEYVID DA COSTA LOPES <deyvid.lopes@aluno.senac.rio>
2025-11-24 21:00:37 -0300

    trabalho final UC3

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\TEMP.SENACRJEDU.013\Trabalho_Final_UC3\Trabalho-Final-UC3-Python\venv
+set VIRTUAL_ENV=C:\Users\TEMP.SENACRJEDU.013\Trabalho_Final_UC3\Trabalho-Final-UC3-Python-1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
